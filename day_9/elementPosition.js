2f8de9a6555d4adfd37e644a34709998e12ca962
OLUWASEUN OLORUNSHOLA <bustlezach001@gmail.com>
2024-02-01 12:45:00 +0100

    search left and right position in js

--- day_9/elementPosition.js
+++ day_9/elementPosition.js
@@ -61,3 +61,4 @@ function firstLastPosition(arr, target) {
 const a = [0, 1, 2, 3, 3, 3, 4, 6];
 console.log(firstLastPosition(a, 3));
 console.log(firstLastPosition([3, 5, 7], 3));
+console.log(firstLastPosition([3, 5, 7], 7));
