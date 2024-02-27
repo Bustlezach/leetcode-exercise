72868e6bbb170d9db20c22f42d91a75cb222ccb7
OLUWASEUN OLORUNSHOLA <bustlezach001@gmail.com>
2024-02-27 07:25:14 +0100

    linkedList

--- day_13/linkedList.js
+++ day_13/linkedList.js
@@ -14,101 +14,100 @@
  * deleteAtIndex(index Delete the indexth node in the linked list, if the index is valid, else nothing happens.
  */
 
-class Node{
+class Node {
   constructor(value) {
-    this.value = value;
-    this.next = null;
+      this.value = value;
+      this.next = null;
   }
 }
 
-class SinglyLinkedList{
-  constructor(){
-    this.head = null;
-    this.tail = null;
-    this.size = 0;
+class SinglyLinkedList {
+  constructor() {
+      this.head = null;
+      this.tail = null;
+      this.size = 0;
   }
 
-  get(index){
-      if(index<0 || index >= this.size) return -1;
-      let counter =0;
+  get(index) {
+      if (index < 0 || index >= this.size) return -1;
+      let counter = 0;
       let current = this.head;
-      while(counter!==index){
+      while (counter !== index) {
           current = current.next;
           counter++;
       }
       return current;
   }
 
-  addAtHead(value){
-    const node = new Node(value);
-    if(!this.head){
-      this.head = node;
-      this.tail = node;
-    } else {
-      this.next = this.head;
-      this.head = node;
-    }
-    this.size++;
-    return this;
-  }
-
-  addAtTail(value){
-    const node = new Node(value);
-    if(!this.head){
-      this.head = node;
-      this.tail = node;
-    } else {
-      this.tail.next = null;
-      this.tail = node;
-    }
-    this.size++;
-    return this;
-  }
-
-  addAtIndex(index, value){
-    if (index < 0 || index > this.size) return 'Invalid index';
-    if(index === this.size) return this.addAtTail(value);
-    if(index === 0) return this.addAtHead(value);
-    let prev = this.get(index-1);
-    let temp = prev.next;
-    prev.next = node;
-    node.next = temp;
-    this.size++;
-    return this;
-  }
-
-  deleteAtlndex(index){
-    if (index < 0 || index >= this.size) return 'Invalid index';
-    if(index === 0){
-      // delete head
-      let temp = this.head;
-      this.head = temp.next;
-      this.size--;
-      if (this.size === 0) {
-        this.tail = null;
+  addAtHead(value) {
+      const node = new Node(value);
+      if (!this.head) {
+          this.head = node;
+          this.tail = node;
+      } else {
+          node.next = this.head;
+          this.head = node;
       }
-      return temp;
-    }
-    if (index === this.size-1) {
-      // delete tail
-      let temp = this.tail;
-      let prev = this.get(index-1);
-      this.tail = prev;
-      prev.next = null;
+      this.size++;
+      return this;
+  }
+
+  addAtTail(value) {
+      const node = new Node(value);
+      if (!this.head) {
+          this.head = node;
+          this.tail = node;
+      } else {
+          this.tail.next = node;
+          this.tail = node;
+      }
+      this.size++;
+      return this;
+  }
+
+  addAtIndex(index, value) {
+      if (index < 0 || index > this.size) return 'Invalid index';
+      if (index === this.size) return this.addAtTail(value);
+      if (index === 0) return this.addAtHead(value);
+      const node = new Node(value);
+      let prev = this.get(index - 1);
+      let temp = prev.next;
+      prev.next = node;
+      node.next = temp;
+      this.size++;
+      return this;
+  }
+
+  deleteAtIndex(index) {
+      if (index < 0 || index >= this.size) return 'Invalid index';
+      if (index === 0) {
+          let temp = this.head;
+          this.head = temp.next;
+          this.size--;
+          if (this.size === 0) {
+              this.tail = null;
+          }
+          return temp;
+      }
+      if (index === this.size - 1) {
+          let temp = this.tail;
+          let prev = this.get(index - 1);
+          this.tail = prev;
+          prev.next = null;
+          this.size--;
+          return temp;
+      }
+      let prev = this.get(index - 1);
+      let deletedNode = prev.next;
+      prev.next = deletedNode.next;
       this.size--;
-      return temp;
-    }
-    let prev = this.get(index-1);
-    let deletedNode = prev.next;
-    prev.next = deletedNode.next;
-    this.size--;
-    return deletedNode;
+      return deletedNode;
   }
 }
 
 const sl = new SinglyLinkedList();
 sl.addAtHead(1);
 sl.addAtTail(2);
-sl.addAtlndex(2, 3);
-// console.log(sl);
+sl.addAtIndex(2, 3);
 console.log(sl.get(2));
+console.log(sl)
